34aaf4c2f291d53cdf0d3ec31e08a6d43fd2a6ad
Akash Dutta <dutta.akash1993official@gmail.com>
2020-08-20 20:35:59 +0530

    Updates

---
+++ precache-manifest.a7236350fee4daee3d791b26bfc76375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3d9973691490407666b06f5945f145",
+    "url": "/kittie-friends/index.html"
+  },
+  {
+    "revision": "424d63994a9b59633ae1",
+    "url": "/kittie-friends/static/css/main.51f34380.chunk.css"
+  },
+  {
+    "revision": "8bfd9a46423adc120756",
+    "url": "/kittie-friends/static/js/2.65eac47e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittie-friends/static/js/2.65eac47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424d63994a9b59633ae1",
+    "url": "/kittie-friends/static/js/main.cf15ae1f.chunk.js"
+  },
+  {
+    "revision": "389d5d0d769c4900d9d5",
+    "url": "/kittie-friends/static/js/runtime-main.e8ff63e4.js"
+  }
+]);
